d4740cbba365f346935f737214bdb6985a00c71e
michiya-59 <nishino.michiya0509@gmail.com>
2024-09-16 14:56:25 +0900

    ホーム画面機能の実装

--- app/controllers/expense_module/home_controller.rb
+++ app/controllers/expense_module/home_controller.rb
@@ -3,10 +3,43 @@
 module ExpenseModule
   class HomeController < ApplicationController
     def index
+      # 現在の年月を取得
+      current_month_start = Date.current.beginning_of_month
+      current_month_end = Date.current.end_of_month
       @current_time = Time.current
+
+      @selected_month = params[:month].present? ? params[:month].to_i : Time.current
+
+      expense_categories = combined_categories_for(ExpenseCategory, current_user.id)
+      # 予想支出の合計
+      @total_expected_amount = expense_categories.sum{|category| category.expected_amount || 0}
+
+      # 支出の合計・収入の合計
+      _, @total_expenses_amount, @total_incomes_amount = calculate_difference_price current_user.id, current_month_start, current_month_end
     end
 
-    def search; end
+    def search
+      @current_time = Time.current
+      selected_month = params[:month].to_i
+      selected_year = @current_time.year
+
+      @selected_month = if params[:month].present?
+                          Time.zone.local(@current_time.year, params[:month].to_i, 1)
+                        else
+                          @current_time
+                        end
+
+      expense_categories = combined_categories_for(ExpenseCategory, current_user.id)
+      # 予想支出の合計
+      @total_expected_amount = expense_categories.sum{|category| category.expected_amount || 0}
+
+      current_month_start = Date.new(selected_year, selected_month, 1).beginning_of_month
+      current_month_end = current_month_start.end_of_month
+
+      _, @total_expenses_amount, @total_incomes_amount = calculate_difference_price(current_user.id, current_month_start, current_month_end)
+
+      render :index
+    end
 
     def export_pdf; end
   end
--- app/javascript/controllers/expense_search.js
+++ app/javascript/controllers/expense_search.js
@@ -1,22 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
   const monthSelect = document.getElementById('month-select');
+  const monthForm = document.getElementById('month-form');
 
   monthSelect.addEventListener('change', function () {
-    const selectedMonth = monthSelect.value;
-
-    // 非同期リクエストを送信
-    fetch(`/expense/home/search?month=${selectedMonth}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
-      }
-    })
-      .then(response => response.json())
-      .then(data => {
-        // 結果を表示
-        document.getElementById('result').innerText = data.message;
-      })
-      .catch(error => console.error('Error:', error));
+    // セレクトボックスが変更されたときにフォームを送信
+    monthForm.submit();
   });
 });
